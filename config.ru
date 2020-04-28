3882255d8a0075c6653b64a1bb397218e8341d06
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-28 16:20:26 +0100

    Sets up testinfrastructure and checks correct text on homepage

--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 # config.ru (run with rackup)
-require './battle.rb'
+require './app.rb'
 run Battle
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,4 +97,14 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  ENV['RACK_ENV'] = 'test'
+  
+  require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+  require 'capybara'
+  require 'capybara/rspec'
+  require 'rspec'
+  
+  Capybara.app = Battle
 end
