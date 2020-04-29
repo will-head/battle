663c28d72a3a829ec884036e5cae49bc14fcaa6c
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-29 15:52:11 +0100

    web_helpers added

--- app.rb
+++ app.rb
@@ -11,9 +11,7 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
-    
-    #@player_1_name = params[:player_1_name]
-    #@player_2_name = params[:player_2_name]
+ 
     redirect to('/play')
   end 
   
@@ -23,6 +21,7 @@ class Battle < Sinatra::Base
     erb :play
   end 
 
+ 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -105,6 +105,7 @@ RSpec.configure do |config|
   require 'capybara'
   require 'capybara/rspec'
   require 'rspec'
+  require 'features/web_helpers'
   
   Capybara.app = Battle
 end
