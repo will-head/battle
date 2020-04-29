3aef910c63e88deecbf3de0549fa2c176fb2ead5
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-29 15:31:02 +0100

    Post/redirect/Get

--- app.rb
+++ app.rb
@@ -1,17 +1,27 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
     
-    puts params
-    erb(:play)
-  end
+    #@player_1_name = params[:player_1_name]
+    #@player_2_name = params[:player_2_name]
+    redirect to('/play')
+  end 
+  
+  get '/play' do 
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end 
 
   # start the server if ruby file executed directly
   run! if app_file == $0
